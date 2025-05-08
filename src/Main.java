5f92ba11473d3436f56fc43953d6e5f20c2585a8
POO <emily.galeas@epn.edu.ec>
2025-05-08 09:44:05 -0500

    Galeas - menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        System.out.println(" Bienvenido!");
 
+        System.out.printf("¿Cuántos productos deseas ingresar? ");
+        int cantidad = SC.nextInt();
+
+        Producto[] productos = new Producto[cantidad];
+        for (int i = 0; i < cantidad; i++) {
+
+            System.out.println("Ingrese el nombre del producto: ");
+            String nombre = SC.next();
+            System.out.println("Ingrese el precio del producto: ");
+            double precio = SC.nextDouble();
+            System.out.println("Ingrese la cantidad del producto: ");
+            int cant_total = SC.nextInt();
+            System.out.printf("¿Su producto tiene descuento? (si/no): ");
+            String opc = SC.next();
+
+            if (opc.equals("si")) {
+                System.out.printf("Ingrese el descuento del producto: ");
+                double descuento = SC.nextDouble();
+            }
+
+            productos[i] = new Producto(nombre, precio, cant_total);
+        }
     }
 }
--- src/Producto.java
+++ src/Producto.java
@@ -1,6 +1,14 @@
 public class Producto {
     private String nombre;
     private double precio;
-    private int cantidad;
-    private double total;
+    private int cant_total;
+
+    public Producto(String nombre, double precio, int cant_total) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cant_total = cant_total; //productos
+    }
+
+
+
 }
